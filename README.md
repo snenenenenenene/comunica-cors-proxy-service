a7b08065b3a24acc6dd539a270d2152cd95c9337
snenenenenenene <sennebels@gmail.com>
2024-06-27 14:30:34 +0200

    added compat for jsonLD

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Cors proxy service
+# Comunica CORS proxy service
 
-Simple service to launch get requests to other websites without having to deal with cors
+Simple service to launch get requests to other websites without having to deal with CORS
 
-## POST /?url=https://www.someurl.com
+## GET /?url=https://www.someurl.com
 
-Responds with the html content of the url in the body
+Responds with the JSONLD content of the url in the body
--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-import { app, query, errorHandler } from 'mu';
-import request from 'request';
+import { app, query, errorHandler } from "mu";
+import request from "request";
 
-app.post('/', function( req, res ) {
+app.get("/", function (req, res) {
   request(req.query.url, function (error, response, body) {
-    res.send(body);
+    if (error) {
+      res.status(500).send("Error fetching the URL");
+      return;
+    }
+
+    const headers = { ...response.headers };
+
+    if (!headers["link"]) {
+      headers["link"] =
+        '<https://example.com/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"';
+    }
+
+    res.set(headers);
+    res.status(response.statusCode).send(body);
   });
 });
